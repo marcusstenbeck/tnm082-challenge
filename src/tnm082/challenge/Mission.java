57d10763c8ab956ff2420d871c33a45aa9ab4fdb
magst683 <magst683@student.liu.se>
2012-03-22 16:37:53 +0100

    Hej hej här kommer lite klasser (Group och Mission)

---
+++ src/tnm082/challenge/Mission.java
@@ -0,0 +1,37 @@
+package tnm082.challenge;
+
+public class Mission {
+	/**
+	 * Kodad av: Magnus S/Markus O
+	 * Task nr:4
+	 * Datum: 2012-03-22
+	 * Estimerad tid: 4h
+	 * Faktisk tid: xh
+	 * Testad/av: Ja/Nej / namn
+	 * Utcheckad/av: Ja/Nej / namn
+	 * @param namn - beskrivning.
+	 * @return namn - beskrivning.
+
+	 */
+	
+	private String name;
+	private String desc;
+	
+	public String getName() {
+		name = "uberuppdraget";
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getDesc() {
+		desc = "sjukt jävla svårt uppdrag";
+		return desc;
+	}
+	
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+}
