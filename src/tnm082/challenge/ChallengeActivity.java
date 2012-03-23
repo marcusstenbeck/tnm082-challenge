33051bd2f9bf18f2850a4f9b877115c6b26eac15
hanhe945 <hanhe945@TP4003-08.student.itn.liu.se>
2012-03-23 14:29:56 +0100

    wasted

--- src/tnm082/challenge/ChallengeActivity.java
+++ src/tnm082/challenge/ChallengeActivity.java
@@ -1,18 +1,25 @@
 package tnm082.challenge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
 public class ChallengeActivity extends Activity {
-    /** Ropad när den aktivitet är först skapad. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        Log.d("koppla", "upp mot fil");
-        dbtest aa = new dbtest();
-        aa.go();
-        Log.d("uppkoppling", "klar");
-    }
+	/** Ropad när den aktivitet är först skapad. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		Log.d("koppla", "upp mot fil");
+		dbtest db = new dbtest();
+		Log.d("Skapat", "databasobjekt");
+		List<Mission> Mlist = new ArrayList();
+		Log.d("Skapat", "Mlist");
+		db.getMissions();
+		Log.d("uppkoppling", "klar");
+
+	}
 }
