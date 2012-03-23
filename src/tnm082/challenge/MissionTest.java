84e3fee44a4049c18295a78dc047c6d74f000a01
marcusstenbeck <marcus.stenbeck@gmail.com>
2012-03-23 12:00:31 +0100

    Uppdaterade MissionTest för att testa nya konstruktorer. Har däremot inte testat så att kopieringskonstruktorn fungerar.

--- src/tnm082/challenge/MissionTest.java
+++ src/tnm082/challenge/MissionTest.java
@@ -3,7 +3,34 @@ package tnm082.challenge;
 import junit.framework.TestCase;
 
 public class MissionTest extends TestCase {
+	
+	public void testMission() {
+		Mission m = new Mission();
+		
+		assertNotNull(m.getName());
+		assertNotNull(m.getDesc());
+		assertNotNull(m.getId());
+	}
 
+	public void testMissionIntStringString() {
+		// Förbered variabler för konstruktorn
+		int theId = 1;
+		String theName = "Team Tiger";
+		String theDesc = "A pretty awesome team of well-trained tigers of code. From the jungle. Boo-yah!";
+		
+		// Skapa objektet
+		Mission m = new Mission(theId, theName, theDesc);
+		
+		// kolla så att konstruktorn har tilldelat alla variabler
+		assertEquals(theId, m.getId());
+		assertEquals(theName, m.getName());
+		assertEquals(theDesc, m.getDesc());
+	}
+/*
+	public void testMissionMission() {
+		fail("Not yet implemented");
+	}
+*/
 	public void testGetId() {
 		Mission m = new Mission();
 		
