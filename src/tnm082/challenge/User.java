3ac68abe3c4c3ceeca905e9e259778eda2ab4008
marcusstenbeck <marcus.stenbeck@gmail.com>
2012-03-23 11:29:20 +0100

    Bytte namn på Users till User.

---
+++ src/tnm082/challenge/User.java
@@ -0,0 +1,85 @@
+package tnm082.challenge;
+
+//import Mission.jaca;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+    * Kodad av: Flaaten	
+    * Task nr: 3
+    * Datum: 2012-03-22
+    * Estimerad tid: 2 h
+    * Faktisk tid: 3 h
+    * Testad/av: Nej
+    * Utcheckad: Nej
+    
+    */
+
+public class User {
+	private String name;
+	private String pass;
+	private List<Mission> do_it;
+	private List<Mission> done_it;
+	
+	public User() {
+		this.name = "klas";
+		this.pass = "kalas";
+		
+		// En lista över uppdrag som ska utföras
+		this.do_it = new ArrayList<Mission>();
+		
+		// En lista över uppdrag som användaren har klarat
+		this.done_it = new ArrayList<Mission>();
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public int getId() {
+		//return hämta id från databasen;
+		return 0;
+	}
+
+	public void doneIt(Mission the_mission)
+	{
+		//b blir true om the mission har tagits bort ur do_it
+		boolean b = do_it.remove(the_mission);
+		
+		if(b)
+			done_it.add(the_mission);
+		
+		/*for(Mission m : do_it)
+		{
+			if(m.getId()==the_mission.getId())
+				do_it.remove(m);//do_it.remove(the_mission.getID()); ska tabort uppdraget från "att göra listan" med hjälp av id som hämtas från databasen
+		}*/
+	
+	}
+	public void add_do(Mission the_mission)
+	{
+		do_it.add(the_mission);
+	
+	}
+	public void remove_do(Mission the_mission)
+	{
+		do_it.remove(the_mission);
+		/*for(Mission m : do_it)
+		{
+			if(m.getId()==the_mission.getId())
+				do_it.remove(m);
+		}*/
+	}
+}
