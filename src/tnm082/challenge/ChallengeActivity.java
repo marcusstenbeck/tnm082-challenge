954be9209077f224f94532385d25af0af42b5f5c
hanhe945 <hanhe945@TP4003-06.student.itn.liu.se>
2012-04-17 11:25:18 +0200

    Döpt om dbtest till DBHandler

--- src/tnm082/challenge/ChallengeActivity.java
+++ src/tnm082/challenge/ChallengeActivity.java
@@ -2,39 +2,49 @@ package tnm082.challenge;
 
 import java.util.ArrayList;
 import java.util.List;
+import android.view.View;
 
 import android.app.Activity;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 import android.util.Log;
 
 
-public class ChallengeActivity extends ListActivity {
+public class ChallengeActivity extends Activity {
     /** Ropad när den aktivitet är först skapad. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.main);
-        
-        final Feed missionFeed = new Feed(); 
-        //TODO lägg in feeden i vyn.
-        
-        setListAdapter(ArrayAdapter.createFromResource(getApplicationContext(),
-                R.array.tut_titles, R.layout.list_item));
         setContentView(R.layout.main);
-		dbtest db = new dbtest();
-		Log.d("SKAPAT", "databasobjekt");
+        
+//        final Feed missionFeed = new Feed(); 
+//        //TODO lägg in feeden i vyn.
+//        
+//        setListAdapter(ArrayAdapter.createFromResource(getApplicationContext(),
+//                R.array.tut_titles, R.layout.list_item));
+        
+
+        final TextView text1 = (TextView)findViewById(R.id.editText1);
+        final TextView text2 = (TextView)findViewById(R.id.editText2);
+        final TextView text3 = (TextView)findViewById(R.id.editText3);
+        
+        
+		DBHandler db = new DBHandler();
 		List<Mission> Mlist = new ArrayList<Mission>();
-		Log.d("SKAPAT", "Mlist");
 		Mlist = db.getMissions();
-		Log.d("UPPKOPPLING", "klar");
+
+		text1.setText(Integer.toString(Mlist.get(0).getId()));
+		text2.setText(Mlist.get(0).getName());
+		text3.setText(Mlist.get(0).getDesc());
 
 		for (Mission m : Mlist)
 		{
 			Log.d("ID",Integer.toString(m.getId()));
 			Log.d("Namn",m.getName());
 			Log.d("Beskrivning",m.getDesc());
+
 		}
     }
 }
