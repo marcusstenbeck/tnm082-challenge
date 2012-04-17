0b73709c8b4ec9921d3a63922adfef086f69a342
Marcus Stenbeck <marcus.stenbeck@gmail.com>
2012-04-17 16:31:18 +0200

    Kommenterat UserTest och AllTests. AndroidManifest.xml uppdaterat för att stödja JUnit Test.

--- src/tnm082/challenge/AllTests.java
+++ src/tnm082/challenge/AllTests.java
@@ -6,10 +6,17 @@ import junit.framework.TestSuite;
 public class AllTests {
 
 	public static Test suite() {
+		// Skapa en ny Testsvit
 		TestSuite suite = new TestSuite(AllTests.class.getName());
+		
 		//$JUnit-BEGIN$
+		
+		// LŠgg till testklass fšr klassen Mission
 		suite.addTestSuite(MissionTest.class);
+		
+		// LŠgg till testklass fšr klassen User
 		suite.addTestSuite(UserTest.class);
+		
 		//$JUnit-END$
 		return suite;
 	}
