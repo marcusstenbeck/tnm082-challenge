b514ace166ac2875abe129be0e5c095a12bdad9a
clurre <marfl109@student.liu.se>
2012-04-19 11:41:58 +0200

    länka den med groupusersactivity

--- src/tnm082/challenge/activities/GroupActivity.java
+++ src/tnm082/challenge/activities/GroupActivity.java
@@ -8,6 +8,7 @@ import tnm082.challenge.Mission;
 import tnm082.challenge.R;
 import tnm082.challenge.R.layout;
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -36,23 +37,36 @@ public class GroupActivity extends ListActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	  super.onCreate(savedInstanceState);
+	  
 	  DBHandler db = new DBHandler();
 	  List<Group> Glist = new ArrayList<Group>();
 	  Glist = db.getGroups();
 	  String[] GROUPS = new String[]{Glist.get(0).getName(),Glist.get(1).getName()};
-	//skapar listan med design som hittas i res/layout/list_item.xml och fylls med data ifrån listan COUNTRIES (se längre ned)
+	  
+	  //skapar listan med design som hittas i res/layout/list_item.xml och fylls med data ifrån listan COUNTRIES (se längre ned)
 	  setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, GROUPS));  
 
+	  
 	  ListView lv = getListView();
 	  lv.setTextFilterEnabled(true);
+	 
 	  
 
 	  lv.setOnItemClickListener(new OnItemClickListener() {
+		  
+		  
+		  
 	    public void onItemClick(AdapterView<?> parent, View view,
 	        int position, long id) {
 	      // When clicked, show a toast with the TextView text
-	      Toast.makeText(getApplicationContext(), ((TextView) view).getText(),
-	          Toast.LENGTH_SHORT).show();
+	      //Toast.makeText(getApplicationContext(), ((TextView) view).getText(),
+	      //    Toast.LENGTH_SHORT).show();
+       	
+
+	     Intent intent = new Intent(view.getContext() , GroupUsersActivity.class);
+	      startActivity(intent);
+	    	
+	    	
 	    }
 	  });
 	}
