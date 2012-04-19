6b3d845854548d619b028a9ea4329d981e3d3693
marol114 <marol114@student.liu.se>
2012-04-19 16:14:14 +0200

    Bara ändrat tillbaka till ickecraschande profil ^^

--- src/tnm082/challenge/activities/UserActivity.java
+++ src/tnm082/challenge/activities/UserActivity.java
@@ -1,16 +1,15 @@
 package tnm082.challenge.activities;
 
+import java.util.ArrayList;
+import java.util.List;
+import tnm082.challenge.DBHandler;
+import tnm082.challenge.Mission;
 import tnm082.challenge.R;
 import android.app.Activity;
-import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 /**
  * Kodad av: Markus Olsson
@@ -27,14 +26,7 @@ public class UserActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user); //Hämtar layout från res/layout/user.xml
         
-  	  //setListAdapter(new ArrayAdapter<String>(this, R.layout.user, listView1));
-  	  
-  	  //ListView mission_list = getListView();
-	  //mission_list.setTextFilterEnabled(true);
-
+        
+        
   	    }
-    
-    static final String[] listView1 = new String[] {
-    	"Current Mission", "Qued Mission 1", "Qued Mission 2", "Qued Mission 3", "Qued Mission 4",
-    	"Qued Mission 5", "Qued Mission 6"};
     }
