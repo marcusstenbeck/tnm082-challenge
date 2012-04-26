7fa3c32cc4f02cbb49df08d3f1ac541aa8228b87
marol114 <marol114@student.liu.se>
2012-04-26 11:18:09 +0200

    Lade till checkbosxlistener

--- src/tnm082/challenge/activities/MissionActivity.java
+++ src/tnm082/challenge/activities/MissionActivity.java
@@ -9,8 +9,28 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.CheckBox;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+/**
+ * Kodad av: Markus Olsson
+ * Task nr: 3 Sprint 2
+ * Datum: 2012-04-25
+ * Estimerad tid: 4h
+ * Faktisk tid: h
+ * Testad/av: Ja/Nej / namn
+ * Utcheckad/av: Ja/Nej / namn
+ */
 
 public class MissionActivity extends Activity {
+	
+	//Skapar min CheckBox CheckBoxDone
+	private CheckBox checkDone;
+	
+	//Skapar OnClickListener till CheckBoxen
+	OnClickListener checkBoxListener;
+	
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.mission_info); //layouten designas i res/layout/main.xml
@@ -32,13 +52,19 @@ public class MissionActivity extends Activity {
 	    // Hämta missions så att vi kan leta upp vår mission
 	    List<Mission> mList = db.getMissions();
 	    
+	    
 // ######## HÄMTA VÅR SPECIFIKA MISSION ########## 
 	    
 	    final TextView nameText = (TextView)findViewById(R.id.textView1);
         
         nameText.setText(contentName);
+        
+        
+      //Koppling mellan Done-knappen och databasen
+
+	}	
 }
-	}
+
 
 //getta ID fˆr valda uppdraget
 //getta name och desc
