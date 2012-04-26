352b4c044119fb0d749ac4065d05d767877a60c6
Marcus Stenbeck <marcus.stenbeck@gmail.com>
2012-04-26 12:01:11 +0200

    Gjort så att layouten för gruppsidan är go. Ingen data infogas; bara dummy!

--- src/tnm082/challenge/activities/GroupUsersActivity.java
+++ src/tnm082/challenge/activities/GroupUsersActivity.java
@@ -4,18 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import tnm082.challenge.DBHandler;
-import tnm082.challenge.User;
 import tnm082.challenge.Group;
 import tnm082.challenge.R;
+import tnm082.challenge.User;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.AdapterView.OnItemClickListener;
-import android.app.ListActivity;
+import android.app.Activity;
 
 /**
  * Kodad av: Flaaten
@@ -28,42 +23,47 @@ import android.app.ListActivity;
  */
 
 
-public class GroupUsersActivity extends ListActivity {
+public class GroupUsersActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	  super.onCreate(savedInstanceState);
-	 // setContentView(R.layout.group_view_of_users); //design som hittas i res/layout/feed_overview.xml
+	  
+	  setContentView(R.layout.single_group);
 
-	  //skapar listor från JSON 
+	  // Skapa en DBHandler fšr att hŠmta data
 	  DBHandler db = new DBHandler();
-	  List<Group> Glist = new ArrayList<Group>();
-	  Glist = db.getGroups();
-	  List<User> Ulist = new ArrayList<User>();
-	  Ulist = db.getUsers();//Notera att detta igentligen skall vara en funktion som hämtar users som tillhör denna gruppen
+	  
+	  // HŠmta en grupps information
+	  Group group;
+	  
+	  // Skapa en list item fšr att hŒlla alla users som Šr med i gruppen
+	  List<User> memberList = new ArrayList<User>();
+	  memberList = db.getUsers();//Notera att detta igentligen skall vara en funktion som hämtar users som tillhör denna gruppen
 	  
 	 //hämtar id:et från groupactivity och sparar den i en string array med usernames
-	  String[] USERS = new String[]{Glist.get(getIntent().getExtras().getInt("id")).getName(),Ulist.get(0).getName(),Ulist.get(1).getName()};
+	  String[] USERS = new String[] {
+			  							"Klasse Dummy",
+										"Dummy Dalton",
+										"Dixie Dummy",
+										"Duh Me",
+										"Klasse Dummy",
+										"Dummy Dalton",
+										"Dixie Dummy",
+										"Duh Me",
+										"Klasse Dummy",
+										"Dummy Dalton",
+										"Dixie Dummy",
+										"Duh Me",
+			  							"Kathult af Dhumey"
+			  						};
 		
 	  //skapar listan
-	  setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, USERS));  
-
-	  ListView lv = getListView();
-	  lv.setTextFilterEnabled(true);
-
-	  lv.setOnItemClickListener(new OnItemClickListener() {
-	    public void onItemClick(AdapterView<?> parent, View view,
-	        int position, long id) {
-	      // When clicked, show a toast with the TextView text
-	      Toast.makeText(getApplicationContext(), Integer.toString(((TextView) view).getId()),
-	          Toast.LENGTH_SHORT).show();
-	    }
-	  });
-	}
-	
-	static final String[] COUNTRIES = new String[] {
-	    "Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra",
-	    "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina",
-	    "Armenia"
-	  };
+	  //setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, USERS));
 	  
+	  // HŠmta listview frŒn XML-layouten
+	  ListView lv = (ListView) findViewById(R.id.listGroupMembers);
+	  
+	  // Bind en ArrayAdapter med en strŠnglista fylld med gruppdatat
+	  lv.setAdapter(new ArrayAdapter<String>(this, R.layout.list_item, USERS));
+	} 
 }
