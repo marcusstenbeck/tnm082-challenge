fea011d2f36d7ebffbede31d56b818a5e1566ffc
kwollter <kwollter@norr-133-149.eduroam.liu.se>
2012-04-26 16:17:09 +0200

    Ändrat user.xml och  UserActivity.java så två listor kan visas på profilsidan.

--- src/tnm082/challenge/activities/UserActivity.java
+++ src/tnm082/challenge/activities/UserActivity.java
@@ -11,7 +11,17 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.ListView;
 
+//--------------------------------------
+
+import android.app.ListActivity;
+import android.net.Uri;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView.OnItemClickListener;
+import android.util.Log;
 
 /**
  * Kodad av: Markus Olsson/Mathias Bergqvist
@@ -31,15 +41,50 @@ public class UserActivity extends Activity {
         DBHandler db = new DBHandler(); //Databashanterar
         
         //Hämtar alla users från databasen.
-        List<User> users = new ArrayList<User>();
-        users = db.getUsers();
-       
+        
+        List<User> users = db.getUsers();
         User tempUser = users.get(0); //Hämtar in for för temporär user, som skall vara sessionuser sen.
         
         //Skriver ut användarens namn som rubrik. 
         final TextView nameText = (TextView)findViewById(R.id.textView2);
         nameText.setText(tempUser.getName());
+        //----------------------------------------------------------
+       /* Test fšr att kolla pŒ en befintlig lista
+        int feedSize = users.size();
+    	  final String[] FEED = new String[feedSize];
+    	  for (int i=0; i<(feedSize); i++)
+    		  FEED[i] = users.get(i).getName();
         
+        ListView lv = (ListView)findViewById(R.id.user_mission_list);
+        lv.setAdapter(new ArrayAdapter<String>(this, R.layout.list_item, FEED));*/
+        //-----------------------------------------------------------
         
+        //Lista accepterade uppdrag
+        //List<Mission> acceptedMList  = new ArrayList<Mission>();
+       // acceptedMList = tempUser.getAcceptedMissions();
+        List<User> acceptedMList  = new ArrayList<User>();
+        acceptedMList = users;
+        int feedSize = acceptedMList.size();
+    	  final String[] FEED = new String[feedSize];
+    	  for (int i=0; i<feedSize; i++)
+    		  FEED[i] = acceptedMList.get(i).getName();
+        //koppla till lista i user.xml
+        ListView lv = (ListView)findViewById(R.id.accepted_mission_list);
+        lv.setAdapter(new ArrayAdapter<String>(this, R.layout.list_item, FEED));
+        
+        //Lista avklarade uppdrag-----------------------------------
+        //List<Mission> completedMList  = new ArrayList<Mission>();
+        //completedMList = tempUser.getAcceptedMissions();
+        List<User> completedMList  = new ArrayList<User>();
+        completedMList = users;
+        
+        int feedSize2 = completedMList.size();
+    	  final String[] FEED2 = new String[feedSize2];
+    	  for (int i=0; i<feedSize2; i++)
+    		  FEED2[i] = completedMList.get(i).getName();
+        //koppla till lista i user.xml
+        ListView lv2 = (ListView)findViewById(R.id.completed_mission_list);
+        lv2.setAdapter(new ArrayAdapter<String>(this, R.layout.list_item, FEED2));
+       
   	    }
     }
